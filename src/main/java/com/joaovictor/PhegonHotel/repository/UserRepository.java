106983df510594b2378b8c87c51da883606021c3
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2024-07-27 19:09:51 -0300

    add repositories

---
+++ src/main/java/com/joaovictor/PhegonHotel/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.joaovictor.PhegonHotel.repository;
+
+import com.joaovictor.PhegonHotel.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+}
